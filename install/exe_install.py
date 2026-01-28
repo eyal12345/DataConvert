fc700a9682eb9452cb6866c08814e855e6d28a2b
eyal12345 <eyal1236@gmail.com>
2026-01-28 14:34:33 +0200

    changed to work_path

--- install/exe_install.py
+++ install/exe_install.py
@@ -18,13 +18,13 @@ def install_application(project_path) -> None:
         result = subprocess.run(application, shell=True, capture_output=True, text=True)
         print(result.stdout)
 
-def transfer_installations(current_path, save_folder) -> None:
+def transfer_installations(work_path, save_folder) -> None:
     os.makedirs(f'{save_folder}/dist') if not os.path.exists(f'{save_folder}/dist') else None
-    shutil.move(f'{current_path}/dist/main.exe', f'{save_folder}/dist/main.exe')
-    shutil.move(f'{current_path}/main.spec', f'{save_folder}/main.spec')
+    shutil.move(f'{work_path}/dist/main.exe', f'{save_folder}/dist/main.exe')
+    shutil.move(f'{work_path}/main.spec', f'{save_folder}/main.spec')
     shutil.rmtree(f'{save_folder}/build') if os.path.exists(f'{save_folder}/build') else None
-    shutil.move(f'{current_path}/build', f'{save_folder}')
-    shutil.rmtree(f'{current_path}/dist')
+    shutil.move(f'{work_path}/build', f'{save_folder}')
+    shutil.rmtree(f'{work_path}/dist')
 
 # get current project path
 project_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -33,8 +33,8 @@ install_dependencies(project_path)
 # install application
 install_application(project_path)
 # get current working directory
-current_path = os.getcwd()
+work_path = os.getcwd()
 # get user's path location from cli
 save_folder = sys.argv[1]
 # transfer all installed files to user's path choice
-transfer_installations(current_path, save_folder)
+transfer_installations(work_path, save_folder)
